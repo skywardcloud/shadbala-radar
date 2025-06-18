6c3e29864dc0ae7490fa318e40164b2b99ab6c84
skywardcloud <info@skywardtechno.com>
2025-06-17 23:17:06 -0400

    Add D3 line chart to frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import * as d3 from 'd3';
 
 function App() {
   const [start, setStart] = React.useState('');
@@ -7,6 +8,7 @@ function App() {
   const [lat, setLat] = React.useState('40.7128');
   const [lon, setLon] = React.useState('-74.0060');
   const [data, setData] = React.useState(null);
+  const svgRef = React.useRef(null);
 
   const submit = async (e) => {
     e.preventDefault();
@@ -14,6 +16,53 @@ function App() {
     const res = await fetch(`http://localhost:8000/balas?${params}`);
     setData(await res.json());
   };
+
+  React.useEffect(() => {
+    if (!data) return;
+    const svg = d3.select(svgRef.current);
+    svg.selectAll('*').remove();
+
+    const width = 600;
+    const height = 300;
+    const margin = { top: 20, right: 30, bottom: 30, left: 40 };
+
+    const startTime = new Date(data.start);
+    const times = data.data.map((_, i) => new Date(startTime.getTime() + i * 5 * 60 * 1000));
+    const components = ['uccha', 'dig', 'kala', 'cheshta', 'naisargika', 'drik'];
+
+    const x = d3.scaleTime()
+      .domain(d3.extent(times))
+      .range([margin.left, width - margin.right]);
+
+    const y = d3.scaleLinear()
+      .domain([0, d3.max(data.data, row => d3.max(components, c => row['Sun'][c]))])
+      .nice()
+      .range([height - margin.bottom, margin.top]);
+
+    const line = d3.line()
+      .x((d, i) => x(times[i]))
+      .y(d => y(d));
+
+    const colors = d3.schemeCategory10;
+
+    components.forEach((comp, idx) => {
+      const values = data.data.map(row => row['Sun'][comp]);
+      svg.append('path')
+        .datum(values)
+        .attr('fill', 'none')
+        .attr('stroke', colors[idx % colors.length])
+        .attr('stroke-width', 1.5)
+        .attr('d', line);
+    });
+
+    svg.append('g')
+      .attr('transform', `translate(0,${height - margin.bottom})`)
+      .call(d3.axisBottom(x));
+
+    svg.append('g')
+      .attr('transform', `translate(${margin.left},0)`)
+      .call(d3.axisLeft(y));
+  }, [data]);
 
   return (
     <div>
@@ -37,6 +86,7 @@ function App() {
         </label>
         <button type="submit">Fetch</button>
       </form>
+      <svg ref={svgRef} width="600" height="300"></svg>
       {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
     </div>
   );
